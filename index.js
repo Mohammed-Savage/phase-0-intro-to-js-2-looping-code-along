bd679962d01c156cde10110d7b62c3100eaab62b
Mohammed Omar <omartechmohammed@gmail.com>
2024-01-01 20:08:56 -0500

    commit tree f315cb2272b33bc8818bddb565bbe5b67c2e9828

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(thankYouMessage);
+    }
+    return messages;
+  }
+  
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+  function countDown(x) {
+    let down = x
+    while (down >= 0) {
+      console.log(down--);
+    }
+  }
+  
+  console.log(countDown(11));
